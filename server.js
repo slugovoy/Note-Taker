4e9e00a0c476b7778327ef434b07503afcd0b6c2
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2020-12-05 15:19:34 -0700

    added method

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const app = express();
-const PORT = 3030;
+const PORT = process.env.PORT || 3030;
 
 // Middleware!
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 require("./routes/apiRoutes.js")(app);
 require("./routes/htmlRoutes.js")(app);
