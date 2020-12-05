db53bff51c5e143f26c89bd1507020462c9f8bd1
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2020-12-05 00:22:24 -0700

    changed functions, orders

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,64 @@
-const tableData = require("../data/tableData");
-const waitinglistData = require("../data/waitinglistData");
+// Variables
+const { readFile } = require("fs");
+const { writeFile } = require("fs");
+const notesDataBase = require("../db/db.json");
 
-module.exports = function(app) {
+// Main function
+module.exports = function (app) {
 
-app.get("/api/tables", function(req, res){
-   res.json(tableData);
-});
+  // Function to write new notes to data base
+  function updateDbJson() {
+    // Write to file method
+    writeFile("./db/db.json",JSON.stringify(notesDataBase), err => {
+      if(err){
+        console.log(err);
+      }
+    })
+  }
+  // Method to get data from data base when requested
+  app.get("/api/notes", function (req, res) {
+    res.json(notesDataBase);
+  });
 
-app.get("/api/waitlist", function(req, res){
-    res.json(waitinglistData);
- });
+  // Method to add(post) new notes to data base and create unique ID for each note.
+  app.post("/api/notes", function (req, res) {
+    let newNote = req.body;
+    let Id = 1;
 
- app.post("/api/tables", function(req, res){
-    console.log(req.body)
-    if (tableData.length < 5) {
-        tableData.push(req.body);
-        res.json(true);
-    } 
-    else {
-        waitinglistData.push(req.body);
-        res.json(false);
+    // Loop to check if note already has id and if so, compare with i
+    for (let i = 1; i < notesDataBase.length; i++) {
+      let oneNote = notesDataBase[i];
+
+      if (oneNote.id > Id) {
+        Id = oneNote.id;
+      }
     }
-    res.send()
- });
+    // Assign new value to id of new note
+    newNote.id = Id + 1;
 
+    // Push new note to data base
+    notesDataBase.push(newNote);
+    // Rewrite  data base file with new array
+    updateDbJson();
+
+    // Send new note back
+    res.json(newNote);
+  });
+
+  // Method to delete note from data base according to it ID
+  app.delete("/api/notes/:id", function (req, res) {
+    // Loop through da base to find note with id that supposed to delete
+    for (let i = 0; i < notesDataBase.length; i++) {
+      if (notesDataBase[i].id == req.params.id) {
+        // Use splice method to divide data base array in 2 parts
+        notesDataBase.splice(i, 1);
+        break;
+      }
+    }
+    // Rewrite  data base file with new array
+    updateDbJson();
+    // Send data to the front end
+    res.json(notesDataBase);
+  });
+ 
 };
