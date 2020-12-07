c0a77e4badd99c7877d20228c7ccec26d2abac4a
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2020-12-06 20:42:35 -0700

    deleted console.log

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,13 +3,11 @@ const path = require("path");
 module.exports = function(app) {
 
 app.get("/notes", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-    console.log("hello");  
+    res.sendFile(path.join(__dirname, "../public/notes.html")); 
  });
 
  app.get("*", function(req, res){
     res.sendFile(path.join(__dirname, "../public/index.html"));
-    console.log("hello hello");
  });
 
 };
